053a4b3d89a298ad260211630655e291b6b52925
AYAMxGANTENG <muhammad.amin1406@gmail.com>
2019-09-12 15:11:45 +0700

    Fixing Back Handler Detail Baris Inspeksi

--- App/Screen/Inspeksi/DetailBaris.js
+++ App/Screen/Inspeksi/DetailBaris.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Image, ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import { Image, ScrollView, StatusBar, StyleSheet, Text, View, BackHandler } from 'react-native';
 import Colors from '../../Constant/Colors'
 import Taskservices from '../../Database/TaskServices'
 import R from 'ramda';
 
 class DetailBaris extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         let params = props.navigation.state.params;
         let baris = R.clone(params.baris);
@@ -31,7 +31,7 @@ class DetailBaris extends React.Component {
             nilaiPupuk: '',
             nilaiPenabur: '',
             hideKriteria: false,
-            barisPembagi:0,
+            barisPembagi: 0,
             baris,
             idInspection,
             path: '',
@@ -41,6 +41,8 @@ class DetailBaris extends React.Component {
             BLOCK_INSPECTION_CODE: BLOCK_INSPECTION_CODE,
             detailType: params.detailType
         };
+
+		this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
     }
 
     static navigationOptions = ({ navigation }) => {
@@ -61,23 +63,34 @@ class DetailBaris extends React.Component {
         }
     };
 
-    componentWillMount(){
+    componentWillMount() {
         this.props.navigation.setParams({
             detailType: this.state.detailType
         });
         this.loadData();
         this.loadGenbaUser();
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
     }
 
-    loadData(){
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
+    }
+
+    //Fumc Handle Back Press (Aminju)
+    handleBackButtonClick() {
+        this.props.navigation.goBack();
+        return true;
+    }
+
+    loadData() {
         var data = Taskservices.findByWithList('TR_BLOCK_INSPECTION_D', ['ID_INSPECTION', 'AREAL'], [this.state.idInspection, this.state.baris]);
-        for(var i=0; i < data.length; i++){
-            this.getValuComponent({compCode: data[i].CONTENT_INSPECTION_CODE, value: data[i].VALUE})
+        for (var i = 0; i < data.length; i++) {
+            this.getValuComponent({ compCode: data[i].CONTENT_INSPECTION_CODE, value: data[i].VALUE })
             this.loadImage(data[i].BLOCK_INSPECTION_CODE)
         }
     }
 
-    loadImage(trCode){
+    loadImage(trCode) {
         let imgBaris = Taskservices.findByWithList('TR_IMAGE', ['TR_CODE', 'STATUS_IMAGE'], [trCode, 'BARIS']);
         let path = '';
         try {
@@ -85,74 +98,74 @@ class DetailBaris extends React.Component {
         } catch (error) {
             path = '';
         }
-        this.setState({path})
+        this.setState({ path })
     }
 
-    getValuComponent(data){
+    getValuComponent(data) {
         let compCode = data.compCode;
-        switch(compCode){
+        switch (compCode) {
             case 'CC0001':
-                this.setState({nilaiJmlPokok: data.value})
+                this.setState({ nilaiJmlPokok: data.value })
                 break;
             case 'CC0002':
-                this.setState({nilaiPokokPanen: data.value})
+                this.setState({ nilaiPokokPanen: data.value })
                 break;
             case 'CC0003':
-                this.setState({nilaiBuahTinggal: data.value})
+                this.setState({ nilaiBuahTinggal: data.value })
                 break;
             case 'CC0004':
-                this.setState({nilaiBrondolPiring: data.value})
+                this.setState({ nilaiBrondolPiring: data.value })
                 break;
             case 'CC0005':
-                this.setState({nilaiBrondolTph: data.value})
+                this.setState({ nilaiBrondolTph: data.value })
                 break;
             case 'CC0006':
-                this.setState({nilaiPokokTdkPupuk: data.value})
+                this.setState({ nilaiPokokTdkPupuk: data.value })
                 break;
             case 'CC0007':
-                this.setState({nilaiPiringan: data.value})
+                this.setState({ nilaiPiringan: data.value })
                 break;
             case 'CC0008':
-                this.setState({nilaiSarkul: data.value})
+                this.setState({ nilaiSarkul: data.value })
                 break;
             case 'CC0009':
-                this.setState({nilaiTph: data.value})
+                this.setState({ nilaiTph: data.value })
                 break;
             case 'CC0010':
-                this.setState({nilaiGwg: data.value})
+                this.setState({ nilaiGwg: data.value })
                 break;
             case 'CC0011':
-                this.setState({nilaiPrun: data.value})
+                this.setState({ nilaiPrun: data.value })
                 break;
             case 'CC0012':
-                this.setState({nilaiTipa: data.value})
+                this.setState({ nilaiTipa: data.value })
                 break;
             case 'CC0013':
-                this.setState({nilaiPenabur: data.value})
+                this.setState({ nilaiPenabur: data.value })
                 break;
             case 'CC0014':
-                this.setState({nilaiPupuk: data.value})
+                this.setState({ nilaiPupuk: data.value })
                 break;
             case 'CC0015':
-                this.setState({nilaiKastrasi: data.value})
+                this.setState({ nilaiKastrasi: data.value })
                 break;
             case 'CC0016':
-                this.setState({nilaiSanitasi: data.value})
+                this.setState({ nilaiSanitasi: data.value })
                 break;
             default:
                 break;
         }
     }
 
-    loadGenbaUser(){
-        if(this.state.detailType === "genba" && this.state.BLOCK_INSPECTION_CODE !== null){
+    loadGenbaUser() {
+        if (this.state.detailType === "genba" && this.state.BLOCK_INSPECTION_CODE !== null) {
             let getData = Taskservices.findBy2('TR_GENBA_INSPECTION', 'BLOCK_INSPECTION_CODE', this.state.BLOCK_INSPECTION_CODE);
             let genbaUser = getData.GENBA_USER;
             let tempData = [];
-            genbaUser.map((data)=>{
+            genbaUser.map((data) => {
                 tempData.push(data)
             });
-            if(tempData.length > 0){
+            if (tempData.length > 0) {
                 this.setState({
                     genbaUser: tempData
                 })
@@ -160,17 +173,17 @@ class DetailBaris extends React.Component {
         }
     }
 
-    renderGenbaUser(){
-            return(
-                this.state.genbaUser.map((data)=>{
-                    return(
-                        <View style={{flex:1, flexDirectmion:'column', justifyContent:'center', paddingVertical: 5}}>
-                            <Text style={{fontSize:14,fontWeight:'600'}}>{data.FULLNAME}</Text>
-                            <Text style={{fontSize:11, color:'#bdbdbd', marginTop:4}}>{data.USER_ROLE}</Text>
-                        </View>
-                    )
-                })
-            )
+    renderGenbaUser() {
+        return (
+            this.state.genbaUser.map((data) => {
+                return (
+                    <View style={{ flex: 1, flexDirectmion: 'column', justifyContent: 'center', paddingVertical: 5 }}>
+                        <Text style={{ fontSize: 14, fontWeight: '600' }}>{data.FULLNAME}</Text>
+                        <Text style={{ fontSize: 11, color: '#bdbdbd', marginTop: 4 }}>{data.USER_ROLE}</Text>
+                    </View>
+                )
+            })
+        )
     }
 
     render() {
@@ -182,93 +195,93 @@ class DetailBaris extends React.Component {
                         barStyle="light-content"
                         backgroundColor={Colors.tintColorPrimary}
                     />
-                    <View style={{alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}}>
+                    <View style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
                         <View style={{ flexDirection: 'row', height: 200 }} >
-                            <Image style={{ width: '100%', height: '100%' }} source={{uri: this.state.path}}></Image>
+                            <Image style={{ width: '100%', height: '100%' }} source={{ uri: this.state.path }}></Image>
                         </View>
                     </View>
                     <View style={styles.section}>
-                        <Text style={[styles.textLokasi, {marginTop:10}]}>Data Penilaian</Text>
-                        <Text style={[styles.textLokasi,{marginTop:5}]}>Baris Ke {this.state.baris}</Text>
+                        <Text style={[styles.textLokasi, { marginTop: 10 }]}>Data Penilaian</Text>
+                        <Text style={[styles.textLokasi, { marginTop: 5 }]}>Baris Ke {this.state.baris}</Text>
                     </View>
                     <View style={styles.section}>
                         <Text style={styles.textTitle}>Perawatan</Text>
                         <View style={styles.lineDivider} />
                         {this.state.nilaiPiringan !== '' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Piringan</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiPiringan}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Piringan</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiPiringan}</Text>
+                            </View>}
                         {this.state.nilaiSarkul !== '' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Pasar Pikul</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiSarkul}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Pasar Pikul</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiSarkul}</Text>
+                            </View>}
                         {this.state.nilaiTph !== '' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>TPH</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiTph}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>TPH</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiTph}</Text>
+                            </View>}
                         {this.state.nilaiGwg !== '' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Gawangan</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiGwg}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Gawangan</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiGwg}</Text>
+                            </View>}
                         {this.state.nilaiPrun !== '' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Prunning</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiPrun}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Prunning</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiPrun}</Text>
+                            </View>}
                         {this.state.nilaiTipa !== '' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Titi Panen</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiTipa}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Titi Panen</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiTipa}</Text>
+                            </View>}
                     </View>
 
                     <View style={styles.section}>
                         <Text style={styles.textTitle}>Panen</Text>
                         <View style={styles.lineDivider} />
                         {this.state.nilaiPokokPanen !== '0' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Pokok Panen</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiPokokPanen}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Pokok Panen</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiPokokPanen}</Text>
+                            </View>}
                         {this.state.nilaiBuahTinggal !== '0' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Buah Tinggal</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiBuahTinggal}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Buah Tinggal</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiBuahTinggal}</Text>
+                            </View>}
                         {this.state.nilaiBrondolPiring !== '0' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Brondolan di Piringan</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiBrondolPiring}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Brondolan di Piringan</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiBrondolPiring}</Text>
+                            </View>}
                         {this.state.nilaiBrondolTph !== '0' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Brondolan di TPH</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiBrondolTph}</Text>
-                        </View> }
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Brondolan di TPH</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiBrondolTph}</Text>
+                            </View>}
                     </View>
 
                     <View style={styles.section}>
                         <Text style={styles.textTitle}>Pemupukan</Text>
                         <View style={styles.lineDivider} />
                         {this.state.nilaiPokokTdkPupuk !== '0' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Pokok Tidak diPupuk</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiPokokTdkPupuk}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Pokok Tidak diPupuk</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiPokokTdkPupuk}</Text>
+                            </View>}
                         {this.state.nilaiPenabur !== '' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Sistem Penaburan</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiPenabur}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Sistem Penaburan</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiPenabur}</Text>
+                            </View>}
                         {this.state.nilaiPupuk !== '' &&
-                        <View style={styles.sectionRow}>
-                            <Text style={styles.textLabel}>Kondisi Pupuk</Text>
-                            <Text style={styles.textContent}>{this.state.nilaiPupuk}</Text>
-                        </View>}
+                            <View style={styles.sectionRow}>
+                                <Text style={styles.textLabel}>Kondisi Pupuk</Text>
+                                <Text style={styles.textContent}>{this.state.nilaiPupuk}</Text>
+                            </View>}
                     </View>
 
                     {
@@ -353,7 +366,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         marginVertical: 20,
         backgroundColor: 'transparent',
-        justifyContent:'center'
+        justifyContent: 'center'
     },
 
 });
