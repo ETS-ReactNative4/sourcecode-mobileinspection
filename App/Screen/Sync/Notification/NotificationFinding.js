de13890e636a251ec1efb05ee4883879ab334137
AYAMxGANTENG <muhammad.amin1406@gmail.com>
2019-09-12 17:09:12 +0700

    Fixing Notif Tugas Baru

--- App/Screen/Sync/Notification/NotificationFinding.js
+++ App/Screen/Sync/Notification/NotificationFinding.js
@@ -10,27 +10,28 @@ export function NotificationFinding(data, user) {
         FINDING_UPDATE_TIME: data.UPDATE_TIME,
         FINDING_CODE: data.FINDING_CODE
     }
-    if (data.UPDATE_USER == '') {
-        if (data.ASSIGN_TO == user.USER_AUTH_CODE && data.INSERT_USER !== user.USER_AUTH_CODE) {
 
-            /* FINDING BARU DI ASSIGN KE USER */
-            let newData = { ...newNotif, NOTIFICATION_TYPE: 0 }
-            TaskServices.saveData('TR_NOTIFICATION', newData);
-        }
-        let createDate = moment(data.INSERT_TIME, "YYYYMMDDHHmmss");
-        let diffDays = today.diff(createDate, 'days');
-        if (diffDays > 6) {
+    if (data.ASSIGN_TO == user.USER_AUTH_CODE && data.INSERT_USER !== user.USER_AUTH_CODE) {
 
-            /* BELUM DI RESPON 7 HARI SETELAH PEMBUATAN */
-            if (data.ASSIGN_TO == user.USER_AUTH_CODE) {
+        /* FINDING BARU DI ASSIGN KE USER */
+        let newData = { ...newNotif, NOTIFICATION_TYPE: 0 }
+        TaskServices.saveData('TR_NOTIFICATION', newData);
+    }
 
-                /* DIASSIGN TAPI BELUM MERESPON */
-                newNotif.NOTIFICATION_TYPE = 2;
-                TaskServices.saveData('TR_NOTIFICATION', newNotif);
-            }
+    let createDate = moment(data.INSERT_TIME, "YYYYMMDDHHmmss");
+    let diffDays = today.diff(createDate, 'days');
+    if (diffDays > 6) {
+        /* BELUM DI RESPON 7 HARI SETELAH PEMBUATAN */
+        if (data.ASSIGN_TO == user.USER_AUTH_CODE) {
+
+            /* DIASSIGN TAPI BELUM MERESPON */
+            newNotif.NOTIFICATION_TYPE = 2;
+            TaskServices.saveData('TR_NOTIFICATION', newNotif);
         }
     }
-    else if (data.INSERT_USER == user.USER_AUTH_CODE
+
+
+    if (data.INSERT_USER == user.USER_AUTH_CODE
         && data.INSERT_USER != data.ASSIGN_TO) {
         if (data.PROGRESS >= 100) {
             /* TEMUAN SUDAH SELESAI */
@@ -42,7 +43,8 @@ export function NotificationFinding(data, user) {
             TaskServices.saveData('TR_NOTIFICATION', newNotif);
         }
     }
-    else if (data.ASSIGN_TO == user.USER_AUTH_CODE) {
+
+    if (data.ASSIGN_TO == user.USER_AUTH_CODE) {
         if (data.PROGRESS >= 100 && data.RATING_VALUE > 0) {
 
             /* YANG DITUGASKAN MENDAPAT RATING */
