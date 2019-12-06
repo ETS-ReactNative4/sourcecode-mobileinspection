69a925a9451cd29a9fe091689f5d109ae2a10862
Kevin Susanto <ulfa.fatmala@bankmandiri.co.id>
2019-12-06 13:57:37 +0700

    update version name

--- App/Screen/More/MoreScreen.js
+++ App/Screen/More/MoreScreen.js
@@ -260,7 +260,7 @@ export default class MoreScreen extends Component {
 
 
             <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 10 }}>
-              <Text>Versi: {DeviceInfo.getVersion()}</Text>
+              <Text>Versi: {DeviceInfo.getVersion()} (A01)</Text>
             </View>
           </View>
 
