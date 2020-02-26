052cb1946e4ed6e8f6e3022676e6dbcd4e61eecc
kevinsusanto <kevinsusanto.lsh@gmail.com>
2020-02-26 09:40:51 +0700

    update realmschema removing trackgps

--- App/Database/RealmSchema.js
+++ App/Database/RealmSchema.js
@@ -45,8 +45,7 @@ let realmSchema = new Realm({
         ModelTables.TR_LOG,
         ModelTables.TM_SUGGESTION_INSPECTION,
         ModelTables.TR_PETAPANEN_HEADER,
-        ModelTables.TR_PETAPANEN_DETAIL,
-        ModelTables.TRACK_GPS
+        ModelTables.TR_PETAPANEN_DETAIL
     ],
 	schemaVersion: ModelTables.SCHEMA_VERSION
 });
