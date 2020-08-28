f9d52d0f75ef33657a7acb5d6274e53cf6ac4771
AYAMxGANTENG <muhammad.amin1406@gmail.com>
2020-08-28 08:33:30 +0700

    Update Menu Peringkat

--- App/Screen/Home/HomeScreen.js
+++ App/Screen/Home/HomeScreen.js
@@ -1213,7 +1213,7 @@ class HomeScreen extends React.Component {
       )
     }
 
-    if (this.state.currentUser.USER_ROLE.includes("EM") || this.state.currentUser.USER_ROLE.includes("SEM_GM")) {
+    if (this.state.currentUser.USER_ROLE.includes("EM") || this.state.currentUser.USER_ROLE.includes("SEM_GM") || this.state.currentUser.USER_ROLE.includes("KEPALA_KEBUN")) {
       return (
         <View style={{ backgroundColor: "white", flexDirection: "row", padding: 15 }}>
 
